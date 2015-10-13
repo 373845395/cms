33928ca49a47189c612ccd1ab06e633b1f1d7a4b
373845395 <373845395@qq.com>
2015-10-13 20:10:55 +0800

    必要文件

---
+++ Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -0,0 +1,16 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>项目管理系统</title>
+	<link rel="stylesheet" type="text/css" href="/mycms/Public/easyui/themes/default/easyui.css">
+	<link rel="stylesheet" type="text/css" href="/mycms/Public/easyui/themes/icon.css">
+	<script type="text/javascript" src="/mycms/Public/easyui/jquery.min.js"></script>
+	<script type="text/javascript" src="/mycms/Public/easyui/jquery.easyui.min.js"></script>
+</head>
+<body class="easyui-layout">
+	<div data-options="region:'north'" style="height:100px;"></div>
+	<div data-options="region:'west',split:true" title="菜单" style="width:200px;"></div>
+	<div data-options="region:'center',title:'Center'"></div>
+</body>
+</html>
